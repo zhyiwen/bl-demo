e41cc1d184bca79541512e8438812d8998b1be03
zhyiwen <zhang92107@sina.com>
2020-04-03 16:00:41 +0800

    安装fontawesome5

--- src/main.js
+++ src/main.js
@@ -9,6 +9,18 @@ import Vant from 'vant'
 import 'vant/lib/index.css'
 import htmlToPdf from './assets/js/htmlToPdf';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText }
+  from '@fortawesome/vue-fontawesome'
+
+library.add(fas, far, fab)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component('font-awesome-layers', FontAwesomeLayers)
+Vue.component('font-awesome-layers-text', FontAwesomeLayersText)
 
 Vue.config.productionTip = false
 Vue.use(Vant);
@@ -17,6 +29,7 @@ Vue.use(vueAxios, axios);
 axios.defaults.withCredentials = false;
 // Vue.prototype.$axios = axios;
 Vue.use(htmlToPdf);
+
 
 new Vue({
   router,
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="box login">
     <div class="logo-cont">
-      <img src="../assets/img/logo.png" alt="">
+      <img src="../assets/img/logo.png" alt="" />
+    </div>
+    <div class="form-cont">
+      <div class="input-group">
+        <input type="text" placeholder="请输入用户名" />
+        <font-awesome-icon icon="user" class="input-icon"></font-awesome-icon>
+      </div>
+      <div class="input-group">
+        <input type="text" placeholder="请输入密码" />
+        <font-awesome-icon icon="key" class="input-icon"></font-awesome-icon>
+      </div>
+      <van-button round block type="info">登录</van-button>
     </div>
   </div>
 </template>
